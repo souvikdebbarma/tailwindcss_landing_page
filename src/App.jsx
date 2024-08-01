72b82d50d13186c46342dec519e0c5cdd2fa1e6d
souvik debbarma <souvikdbarma23@gmail.com>
2024-08-01 15:22:03 +0530

    initialized

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-zinc-400 min-h-screen px-6 py-3 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between">
+      <div className="bg-zinc-400 min-h-screen px-6 py-3 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-16 lg:py-10">
         <Header/>
         <Body/>
         <Footer/>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,16 @@ const Header = () => {
         <img src="./assets/Logo.svg" alt="logo" />
         <button className="bg-gradient-to-r from-cyan-500 to-blue-300 text-xs text-white px-2.5 py-1.5 rounded-2xl">Hoster is hiring</button>
       </div>
-      <div>
+      <ul className="hidden lg:flex justify-between items-centerfont-lato text-gray-100 gap-6">
+        <li><a href="#">Plans</a></li>
+        <li><a href="#">Find Domain</a></li>
+        <li><a href="#">Why Hoster</a></li>
+      </ul>
+      <div className="hidden lg:flex justify-center items-center font-lato gap-6">
+        <a className="text-gray-100" href="#">Sign In</a>
+        <button className="rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white">Join Waitlist</button>
+      </div>
+      <div className="lg:hidden">
         <FaBarsStaggered />
       </div>
     </div>
